dd986e8acac226a0303c5d1f9a233fc9c80d1b0e
unknown <you@example.com>
2023-05-19 12:46:43 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,83 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-f",
+      "name": "F",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "yaw": -2.6399205654782687,
+        "pitch": 0.16679563341411452,
+        "fov": 1.3934346732050444
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.903448811738974,
+          "pitch": 0.053075456071184135,
+          "rotation": 0,
+          "target": "1-e"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-e",
+      "name": "E",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": 0.07262821726040869,
+        "pitch": 0.0744405835435078,
+        "fov": 1.3934346732050444
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0195697250627322,
+          "pitch": 0.046164385895140825,
+          "rotation": 0.7853981633974483,
+          "target": "0-f"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "3rdfloor Final Final ",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
